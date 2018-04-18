66ce59393f13e69b6c3d63bfcb56d2d482c87e95
Dimitris Deligiorgis <ddeligiorgis@vivawallet.com>
2018-04-19 00:29:02 +0300

    span lab impl

---
+++ Span_Lab/Span_Lab/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes.Jobs;
+using BenchmarkDotNet.Running;
+
+namespace Span_Lab
+{
+    [CoreJob]
+    [MemoryDiagnoser]
+    public class StringTest
+    {
+        [Params(null, "  foo bar", "   The quick brown fox jumps over the lazy dog")]
+        public string Inputs;
+
+        [Benchmark]
+        public string SafeTrimStart()
+        {
+            string text = Inputs;
+
+            if (string.IsNullOrWhiteSpace(text)) {
+                return text;
+            }
+
+            return text.TrimStart();
+        }
+
+        [Benchmark]
+        public ReadOnlySpan<char> SafeTrimStartSpan()
+        {
+            ReadOnlySpan<char> text = Inputs.AsSpan();
+
+            if (text.IsEmpty) {
+                return text;
+            }
+
+            int i = 0;
+            char c;
+
+            while ((c = text[i]) == ' ') {
+                i++;
+            }
+
+            return text.Slice(i);
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<StringTest>();
+        }
+    }
+}
